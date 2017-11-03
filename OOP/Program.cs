31b9baa398ccbd731efc6767143f3cc8f2372533
gejdos <33354595+gejdos@users.noreply.github.com>
2017-11-03 17:42:08 +0100

    Update Program.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,7 +10,8 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("nazdar");
+            
         }
     }
 }
